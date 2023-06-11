b19446cf18d9c3862ac8640b145914b9f0bd9cb3
Sonny <holasonnycarlos@gmail.com>
2023-06-11 15:51:01 +0800

    set up middleware

--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,6 @@
+import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
-import mongoose from 'mongoose'
 
 import dbConnect from './config/database.js'
 import { userRoutes } from './routes/index.js'
@@ -10,11 +10,15 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 5000
 
+// Database Connection
 dbConnect()
 
+// Middleware
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+// User Routes
 app.use('/api/user', userRoutes)
 
 app.get('/api/hello', (req, res) => {
--- front-end/src/config/axios.js
+++ front-end/src/config/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://http://localhost:8000/api'
+axios.defaults.baseURL = 'http://localhost:8000/api'
 axios.defaults.headers.common['Accept'] = 'application/json'
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
 
