57d2ef74a92e5b2c8c1b8668bc8aadc6cb67c4d9
Sonny <holasonnycarlos@gmail.com>
2023-07-03 10:58:07 +0800

    refactored axios config in the front-end

--- front-end/src/config/axios.js
+++ front-end/src/config/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
+import { domain } from '../constants'
 
-axios.defaults.baseURL = 'http://localhost:8000/api'
+axios.defaults.baseURL = `${domain}api`
 axios.defaults.headers.common['Accept'] = 'application/json'
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
 axios.defaults.withCredentials = true
--- front-end/src/constants/domain.js
+++ front-end/src/constants/domain.js
@@ -1,3 +1,3 @@
-const domain = 'http://localhost:8000'
+const domain = 'https://sr-gate-pass.onrender.com/'
 
 export default domain
